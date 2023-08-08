35a4fd4496baca571371749570fac6fa27c328ab
Sheikh Arfin Ahamed <136627638+SheikhArfin0@users.noreply.github.com>
2023-08-08 09:16:07 +0600

    CPP File

---
+++ area_triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int base, height ,area ;
+
+        cout <<"Enter the base: ";
+        cin >>base ;
+
+        cout<<"Enter the height: ";
+        cin>>height;
+
+        area=0.5*base*height;
+
+        cout<<"Area Of Triangle: "<<area;
+    return 0 ;
+}
